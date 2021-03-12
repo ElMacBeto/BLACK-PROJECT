080c4ae26bb021bb107e14d25e81e13286564915
ElMacBeto <44098805+ElMacBeto@users.noreply.github.com>
2021-03-12 14:13:33 -0700

    corregi efecto carrusel, dimensiones de imagenes

--- scripts/styles.js
+++ scripts/styles.js
@@ -1,3 +1,10 @@
 
-$('.carousel-item img').addClass('img-fit');;
+$('.carousel-item img').addClass('img-fit');
 $('.nav-item').addClass('center-item');
+$('.mynav').css({background: 'blue'});
+$('.mynav').css('box-shadow', '0 5px 10px black');
+$('.mynav a').css('color', 'white');
+
+
+
+
