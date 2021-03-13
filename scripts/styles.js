6db640ff1e16f902a6a5552f910487eea43d4b14
ElMacBeto <44098805+ElMacBeto@users.noreply.github.com>
2021-03-12 18:23:50 -0700

    menu-sticky
    add new effect sticky menu

--- scripts/styles.js
+++ scripts/styles.js
@@ -1,10 +1,26 @@
 
+
+const $menu = $('.mynav');
 $('.carousel-item img').addClass('img-fit');
 $('.nav-item').addClass('center-item');
-$('.mynav').css({background: 'blue'});
-$('.mynav').css('box-shadow', '0 5px 10px black');
+$menu.css({background: 'blue'});
+$menu.css('box-shadow', '0 5px 10px black');
 $('.mynav a').css('color', 'white');
 
+// efecto navbar
+const YMenu = document.getElementById('myMenu');
+const YnavMenu = document.getElementById('navMenu');
+const Yscroll = YMenu.offsetTop - YnavMenu.offsetHeight;
 
-
-
+window.onscroll = function(){
+    const yOffset = window.pageYOffset;
+    
+    if(yOffset > Yscroll){
+        //console.log(YnavMenu.offsetHeight);
+        YMenu.classList.add('sticky-menu');
+    } else{
+        YMenu.classList.remove('sticky-menu');
+    };
+    
+    //console.log(`window: ${yOffset} y Menu:${YMenu - YnavMenu.offsetHeight}`);
+};
